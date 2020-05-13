f2b7de678d3c85f1acb4d051061ae23c8fcd04ba
Bassam AL SAEDY <basaidi@gmail.com>
2020-05-13 08:52:24 +0200

    Intial add

---
+++ controllers/controller.js
@@ -0,0 +1,43 @@
+"use strict";
+// Variables
+var car;
+var plate = document.getElementById('plate');
+var brand = document.getElementById('brand');
+var color = document.getElementById('color');
+var tireBrand = document.getElementById('rueda1');
+var diameter = document.getElementById('diameter1');
+var tireBrand2 = document.getElementById('rueda2');
+var diameter2 = document.getElementById('diameter2');
+var tireBrand3 = document.getElementById('rueda3');
+var diameter3 = document.getElementById('diameter3');
+var tireBrand4 = document.getElementById('rueda4');
+var diameter4 = document.getElementById('diameter4');
+// Create Car
+function createCar() {
+    var car = new Car(plate.value, brand.value, color.value);
+    // Validating that vehicle the plate has 4 digits(d{4}) and 3 letters!
+    var isPlateOK = /^\d{4} ?[A-Za-z]{3}$/.test(plate);
+    if (!isPlateOK) {
+        alert('Please enter a valid plate number!');
+    }
+    else {
+        // Output message
+        var result = document.getElementById('output');
+        result.innerHTML = "Car: <br> <br> Plate: " + car.plate + " | Brand: " + car.brand + " | Color: " + car.color;
+    }
+}
+// Add Wheel
+function addWheels() {
+    if (+diameter.value < 0.4 || +diameter.value > 2) {
+        alert('Diameter should be greater than 0.4 and less than or equal to 2!');
+    }
+    else {
+        var wheels = new Wheel(tireBrand.value, +diameter.value);
+        var wheels2 = new Wheel(tireBrand2.value, +diameter2.value);
+        var wheels3 = new Wheel(tireBrand3.value, +diameter3.value);
+        var wheels4 = new Wheel(tireBrand4.value, +diameter4.value);
+        // Output Wheels message
+        var result = document.getElementById('output2');
+        result.innerHTML = "Wheels: <br> <br> Wheel 1:  Brand: " + wheels.brand + " Diameter: " + wheels.diameter + " | Wheel 2:  Brand: " + wheels2.brand + " Diameter: " + wheels2.diameter + " <br> Wheel 3:  Brand: " + wheels3.brand + " Diameter: " + wheels3.diameter + " | Wheel 4:  Brand: " + wheels4.brand + " Diameter: " + wheels4.diameter;
+    }
+}
---
+++ controllers/controller.ts
@@ -0,0 +1,51 @@
+// Variables
+let car: Car;
+
+let plate: any = document.getElementById('plate')! as HTMLInputElement;
+let brand = document.getElementById('brand')! as HTMLInputElement;
+let color = document.getElementById('color')! as HTMLInputElement;
+
+let tireBrand = document.getElementById('rueda1')! as HTMLInputElement;
+let diameter = document.getElementById('diameter1')! as HTMLInputElement;
+
+let tireBrand2 = document.getElementById('rueda2')! as HTMLInputElement;
+let diameter2 = document.getElementById('diameter2')! as HTMLInputElement;
+
+let tireBrand3 = document.getElementById('rueda3')! as HTMLInputElement;
+let diameter3 = document.getElementById('diameter3')! as HTMLInputElement;
+
+let tireBrand4 = document.getElementById('rueda4')! as HTMLInputElement;
+let diameter4 = document.getElementById('diameter4')! as HTMLInputElement;
+
+// Create Car
+function createCar() {
+    let car = new Car(plate.value, brand.value, color.value);
+
+    // Validating that vehicle the plate has 4 digits(d{4}) and 3 letters!
+    let isPlateOK = /^\d{4} ?[A-Za-z]{3}$/.test(plate);
+
+    if (!isPlateOK) {
+        alert('Please enter a valid plate number!');
+    } else {
+        // Output message
+        let result = document.getElementById('output') as HTMLInputElement;
+        result.innerHTML = `Car: <br> <br> Plate: ${car.plate} | Brand: ${car.brand} | Color: ${car.color}`;
+    }
+}
+
+// Add Wheel
+function addWheels() {
+    if (+diameter.value < 0.4 || +diameter.value > 2) {
+        alert('Diameter should be greater than 0.4 and less than or equal to 2!');
+    } else {
+
+        let wheels = new Wheel(tireBrand.value, +diameter.value);
+        let wheels2 = new Wheel(tireBrand2.value, +diameter2.value);
+        let wheels3 = new Wheel(tireBrand3.value, +diameter3.value);
+        let wheels4 = new Wheel(tireBrand4.value, +diameter4.value);
+
+        // Output Wheels message
+        let result = document.getElementById('output2') as HTMLInputElement;
+        result.innerHTML = `Wheels: <br> <br> Wheel 1:  Brand: ${wheels.brand} Diameter: ${wheels.diameter} | Wheel 2:  Brand: ${wheels2.brand} Diameter: ${wheels2.diameter} <br> Wheel 3:  Brand: ${wheels3.brand} Diameter: ${wheels3.diameter} | Wheel 4:  Brand: ${wheels4.brand} Diameter: ${wheels4.diameter}`;
+    }
+}
